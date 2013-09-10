0c5494495a05445eec8d30898663628d1ceff21b
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2013-09-09 18:14:48 -0700

    Getting a bit more information on relative code sizes.

--- compareSizes.lua
+++ compareSizes.lua
@@ -14,8 +14,20 @@ local function getStanFileLineCount(filename)
 	return lc
 end
 
+local function getTerraFileLineCount(filename)
+	util.wait(string.format("python decomment.py %s decomment_tmp lua", filename))
+	local lc = getLineCount("decomment_tmp")
+	util.wait("rm -f decomment_tmp")
+	return lc
+end
+
+-- Size of Terra AD code
+print(string.format("Terra AD line count: %d", getTerraFileLineCount("ad.t")))
+
+print("----------")
+
 -- Size of Stan AD code
-local filenames = 
+local basefilenames = 
 {
 	"chainable.hpp",
 	"vari.hpp",
@@ -32,12 +44,54 @@ local filenames =
 	"op/dvd_vari.hpp",
 	"op/ddv_vari.hpp"
 }
+local opfilenames = 
+{
+	"operator_addition.hpp",
+	"operator_subtraction.hpp",
+	"operator_multiplication.hpp",
+	"operator_division.hpp",
+	"operator_unary_negative.hpp",
+	"operator_equal.hpp",
+	"operator_less_than.hpp",
+	"operator_less_than_or_equal.hpp",
+	"operator_greater_than.hpp",
+	"operator_greater_than_or_equal.hpp",
+	"acos.hpp",
+	"acosh.hpp",
+	"asin.hpp",
+	"asinh.hpp",
+	"atan.hpp",
+	"atan2.hpp",
+	"ceil.hpp",
+	"cos.hpp",
+	"cosh.hpp",
+	"exp.hpp",
+	"fabs.hpp",
+	"floor.hpp",
+	"fmax.hpp",
+	"fmin.hpp",
+	"log.hpp",
+	"log10.hpp",
+	"pow.hpp",
+	"round.hpp",
+	"sin.hpp",
+	"sinh.hpp",
+	"sqrt.hpp",
+	"tan.hpp",
+	"tanh.hpp"
+}
 local unaryExample = "sqrt.hpp"
 local binaryExample = "pow.hpp"
 local baseCodeLineCount = 0
-for i,f in ipairs(filenames) do
+for i,f in ipairs(basefilenames) do
 	baseCodeLineCount = baseCodeLineCount + getStanFileLineCount(f)
 end
+local opsLineCount = 0
+for i,f in ipairs(opfilenames) do
+	opsLineCount = opsLineCount + getStanFileLineCount(f)
+end
 print(string.format("stan AD base code line count: %d", baseCodeLineCount))
-print(string.format("stan additional unary op line count: ~%d", getStanFileLineCount(unaryExample)))
-print(string.format("stan additional binary op line count: ~%d", getStanFileLineCount(binaryExample)))
+print(string.format("stan AD op code line count: %d", opsLineCount))
+print(string.format("stan AD total line count: %d", baseCodeLineCount + opsLineCount))
+print(string.format("stan incremental unary op line count: ~%d", getStanFileLineCount(unaryExample)))
+print(string.format("stan incremental binary op line count: ~%d", getStanFileLineCount(binaryExample)))
